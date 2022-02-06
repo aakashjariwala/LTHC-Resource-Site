86995e69ccd7f8cb6b3f16f19e9b6f70602d398d
vivCoding <vivCoding@gmail.com>
2022-02-06 00:52:36 -0500

    basic graphql

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,15 @@
 import { ApolloServer } from 'apollo-server'
 import { config } from 'dotenv'
+import schema from './graphql/schema'
 import MongoConnection from './database/config/connection'
 
 const init = async () => {
   try {
     await MongoConnection()
 
-    // const server = new ApolloServer({})
-    // const { url } = await server.listen()
-    // console.log(`Server running at ${url}`)
+    const server = new ApolloServer({ schema })
+    const { url } = await server.listen()
+    console.log(`Server running at ${url}`)
   } catch (e) {
     console.error(e)
   }
