c8809dada581894048667219d0e5cd94957e0892
Jacob Zhang <jacobzhang132@gmail.com>
2022-03-03 22:37:33 -0500

    remove unneccessary imports

--- frontend/src/components/About/index.jsx
+++ frontend/src/components/About/index.jsx
@@ -1,5 +1,4 @@
 import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
