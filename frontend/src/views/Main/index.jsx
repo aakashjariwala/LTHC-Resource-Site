3b3fc5c2dce777b06ea56b61ea2a655ef41a137f
Jacob Zhang <jacobzhang132@gmail.com>
2022-04-02 13:07:51 -0400

    added context for site data

--- frontend/src/views/Main/index.jsx
+++ frontend/src/views/Main/index.jsx
@@ -1,10 +1,13 @@
 import { Container } from '@mui/material'
-import { useEffect } from 'react'
+import { createContext, useContext, useEffect } from 'react'
 import FirstView from '../../components/About'
 import RequirementAccordion from '../../components/RequirementAccordion'
 import Resources from '../../components/Resources'
 import Contacts from '../../components/Contacts'
 import SiteData from '../../hooks/SiteData'
+
+const SiteDataContext = createContext()
+export const useSiteDataContext = () => useContext(SiteDataContext)
 
 function Main() {
   const siteData = SiteData()
@@ -12,12 +15,14 @@ function Main() {
   useEffect(() => {}, [])
 
   return (
-    <Container sx={{ py: 5 }}>
-      <FirstView />
-      <RequirementAccordion />
-      <Resources />
-      <Contacts />
-    </Container>
+    <SiteDataContext.Provider value={siteData}>
+      <Container sx={{ py: 5 }}>
+        <FirstView />
+        <RequirementAccordion />
+        <Resources />
+        <Contacts />
+      </Container>
+    </SiteDataContext.Provider>
   )
 }
 
