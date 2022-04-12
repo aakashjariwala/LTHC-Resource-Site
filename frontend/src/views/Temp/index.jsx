aa426f2c0df3115c5b50fdb00f87e886198435fc
Jacob Zhang <jacobzhang132@gmail.com>
2022-04-12 11:35:53 -0400

    small update

--- frontend/src/views/Temp/index.jsx
+++ frontend/src/views/Temp/index.jsx
@@ -13,5 +13,38 @@ export default function Temp() {
   const [file, setFile] = useState()
   const [isUploading, setIsUploading] = useState(false)
   const [showSuccess, setShowSuccess] = useState(false)
-  return <div>hi</div>
+
+  const onSubmit = async () => {}
+
+  const onFileChange = (e) => {}
+
+  return (
+    <Container sx={{ py: 4 }}>
+      <Typography variant="h6">Upload PDF to modify website</Typography>
+
+      <Box mt={2}>
+        <input
+          type="file"
+          accept=".pdf"
+          onChange={onFileChange}
+          ref={inputRef}
+        />
+      </Box>
+      <Box mt={3}>
+        {(() => {
+          if (isUploading)
+            return showSuccess ? (
+              <Typography>Success!</Typography>
+            ) : (
+              <CircularProgress />
+            )
+          return (
+            <Button variant="contained" onClick={onSubmit}>
+              Upload
+            </Button>
+          )
+        })()}
+      </Box>
+    </Container>
+  )
 }
