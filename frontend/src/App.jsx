73d49ed0fc54aefad1eaea3428d9b300c078c8cb
Jacob Zhang <jacobzhang132@gmail.com>
2022-04-12 03:01:51 -0400

    small change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import Main from './views/Main'
 import UploadPdf from './views/UploadPdf'
+import Temp from './views/temp'
 
 function App() {
   const theme = createTheme({
@@ -24,6 +25,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/upload" exact element={<UploadPdf />} />
+            <Route path="/temp" exact element={<Temp />} />
           </Routes>
         </BrowserRouter>
       </ThemeProvider>
