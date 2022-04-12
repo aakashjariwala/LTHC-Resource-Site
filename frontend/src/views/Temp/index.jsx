1faf77cc9e53e40c795f5c190202c33108e1b490
Jacob Zhang <jacobzhang132@gmail.com>
2022-04-12 11:33:43 -0400

    small update

--- frontend/src/views/Temp/index.jsx
+++ frontend/src/views/Temp/index.jsx
@@ -1,3 +1,17 @@
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  Typography,
+} from '@mui/material'
+import { useEffect, useRef, useState } from 'react'
+import { uploadPDF } from '../../hooks/useFirebase'
+
 export default function Temp() {
+  const inputRef = useRef(null)
+  const [file, setFile] = useState()
+  const [isUploading, setIsUploading] = useState(false)
+  const [showSuccess, setShowSuccess] = useState(false)
   return <div>hi</div>
 }
