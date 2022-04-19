18b06d3770cfe7b9844564c735a8f38ce2b509c1
Jacob Zhang <jacobzhang132@gmail.com>
2022-04-18 22:22:18 -0400

    updated firebase

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -2,15 +2,16 @@ import { initializeApp } from 'firebase/app'
 import { getStorage, ref, getDownloadURL, uploadBytes } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: 'lthc-343004.firebaseapp.com',
-  projectId: 'lthc-343004',
-  storageBucket: 'lthc-343004.appspot.com',
-  messagingSenderId: '242225535777',
-  appId: process.env.REACT_APP_APPID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 }
 
-if (!process.env.REACT_APP_APIKEY || !process.env.REACT_APP_APPID)
+if (!process.env.REACT_APP_API_KEY || !process.env.REACT_APP_APP_ID)
   console.log('missing firebase keys!')
 
 const app = initializeApp(firebaseConfig)
