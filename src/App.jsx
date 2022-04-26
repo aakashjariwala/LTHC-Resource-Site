c1035cbac41b52b1c339be120752d77d91fae7cc
Jacob Zhang <jacobzhang132@gmail.com>
2022-04-26 02:11:58 -0400

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+import {
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+} from '@mui/material'
 import Main from './views/Main'
 import UploadPdf from './views/UploadPdf'
+import Footer from './components/Footer'
 
 function App() {
   const theme = createTheme({
@@ -17,16 +23,27 @@ function App() {
     },
   })
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" exact element={<Main />} />
-          <Route path="/upload-pdf" exact element={<UploadPdf />} />
-          <Route path="*" exact element={<h6>404 Page Not Found</h6>} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" exact element={<Main />} />
+            <Route path="/upload-pdf" exact element={<UploadPdf />} />
+            <Route
+              path="*"
+              exact
+              element={
+                <Typography variant="h5" sx={{ pt: 5, textAlign: 'center' }}>
+                  404 Page Not Found
+                </Typography>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+      <Footer />
+    </>
   )
 }
 
